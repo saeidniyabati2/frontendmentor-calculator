cd1b47af7a004591e7c20d6665f86bff0fc1155b
saeidniyabati2 <saeidniyabati@gmail.com>
2023-02-13 18:23:46 +0330

    adding color for theme 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,43 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm: "375px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        // THEME 1
+        color1: " hsl(222, 26%, 31%)",
+        color2: "hsl(223, 31%, 20%)",
+        color3: " hsl(224, 36%, 15%)",
+        color4: "hsl(225, 21%, 49%) ",
+        color5: "hsl(224, 28%, 35%)",
+        color6: " hsl(6, 63%, 50%)",
+        color7: "hsl(6, 70%, 34%)",
+        color8: "hsl(30, 25%, 89%)",
+        color9: "hsl(28, 16%, 65%)",
+        color10: "hsl(221, 14%, 31%)",
+        color11: "hsl(0, 0%, 100%)",
+        // THEME2
+        // color12: " ",
+        // color13: " ",
+        // color14: " ",
+        // color15: " ",
+        // color16: " ",
+        // color17: " ",
+        // color18: " ",
+        // color19: " ",
+        // color20: " ",
+        // color21: " ",
+        // color22: " ",
+      },
+      fontFamily: {
+        sans: ["Outfit"],
+      },
+    },
   },
   plugins: [],
 };
